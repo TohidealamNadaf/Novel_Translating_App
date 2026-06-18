5e0888ad2796f242750ab670bcc0368310763420
Tohidealam Nadaf <tohidealamfnadaf2003@gmail.com>
2026-06-18 18:00:48 +0530

    bug fixed

--- download_wasm.dart
+++ download_wasm.dart
@@ -1,16 +1,14 @@
 import 'dart:io';
 
 Future<void> main() async {
-  final baseUrl = 'https://github.com/simolus3/sqlite3.dart/raw/main/sqlite3/assets';
-  
   print('Downloading sqlite3.wasm...');
-  final wasmReq = await HttpClient().getUrl(Uri.parse('${baseUrl}/sqlite3.wasm'));
+  final wasmReq = await HttpClient().getUrl(Uri.parse('https://github.com/simolus3/sqlite3.dart/releases/download/sqlite3-2.4.6/sqlite3.wasm'));
   final wasmRes = await wasmReq.close();
   final wasmFile = File('web/sqlite3.wasm');
   await wasmRes.pipe(wasmFile.openWrite());
   
   print('Downloading sqflite_sw.js from tekartik repo...');
-  final swReq = await HttpClient().getUrl(Uri.parse('https://raw.githubusercontent.com/tekartik/sqflite/master/sqflite_common_ffi_web/lib/assets/sqflite_sw.js'));
+  final swReq = await HttpClient().getUrl(Uri.parse('https://raw.githubusercontent.com/tekartik/sqflite/master/packages/sqflite_common_ffi_web/lib/assets/sqflite_sw.js'));
   final swRes = await swReq.close();
   final swFile = File('web/sqflite_sw.js');
   await swRes.pipe(swFile.openWrite());
