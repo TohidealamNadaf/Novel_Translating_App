85432d7d6bb2e686638fe0b73a3ec093088575f6
Tohidealam Nadaf <tohidealamfnadaf2003@gmail.com>
2026-06-18 17:55:42 +0530

    bug fixed

---
+++ download_wasm.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final baseUrl = 'https://github.com/simolus3/sqlite3.dart/raw/main/sqlite3/assets';
+  
+  print('Downloading sqlite3.wasm...');
+  final wasmReq = await HttpClient().getUrl(Uri.parse('${baseUrl}/sqlite3.wasm'));
+  final wasmRes = await wasmReq.close();
+  final wasmFile = File('web/sqlite3.wasm');
+  await wasmRes.pipe(wasmFile.openWrite());
+  
+  print('Downloading sqflite_sw.js from tekartik repo...');
+  final swReq = await HttpClient().getUrl(Uri.parse('https://raw.githubusercontent.com/tekartik/sqflite/master/sqflite_common_ffi_web/lib/assets/sqflite_sw.js'));
+  final swRes = await swReq.close();
+  final swFile = File('web/sqflite_sw.js');
+  await swRes.pipe(swFile.openWrite());
+  
+  print('Done! Files downloaded to web folder.');
+}
